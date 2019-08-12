0cc7e82d3869c1017f0f0156048b343003b80093
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-12 17:57:41 -0400

    Update run.py
    added sleep method to slow things down

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from src import player
 from src import enemy
 from src.manager import Manager
-
+import time
 from colored import fg, bg, attr
 
 #tree: ============ entity_class ===============
@@ -14,10 +14,13 @@ gm.render_status()
 
 while gm.player_1.health > 0 and gm.enemy_ai.health > 0:
     gm.game_over = False 
-    gm.enemy_ai.enemy_do_dmg(gm.player_1)
+    time.sleep(1)
+    gm.enemy_do_dmg(gm.player_1)
+    time.sleep(1)
     gm.input_management()
+    time.sleep(1)
     gm.render_status()
-
+    time.sleep(1)
 gm.game_over = True
 while gm.game_over == True:
     gm.print_end_screen()
