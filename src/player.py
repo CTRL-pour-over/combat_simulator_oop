84639a3758476bf1445f746ebdedcc4cf4717ecc
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-12 17:58:28 -0400

    Update player.py
    Added an __init__ method for the player class.

--- src/player.py
+++ src/player.py
@@ -1,6 +1,12 @@
 from src.entity import entity
 
 class player_child(entity):
-    name = input("Enter Your Name: ")
+    name = "Default Player"
     experience = 0
+
+    def __init__(self, arg_name = "", arg_health = 100, arg_damage = 50, arg_defense = 50):
+        self.name = arg_name
+        self.health = arg_health
+        self.damage = arg_damage
+        self.defense = arg_defense
 
