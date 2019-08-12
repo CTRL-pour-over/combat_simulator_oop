08e34a352894d7af7228a89b1f731fc939e7f546
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-12 18:00:23 -0400

    Update entity.py
    Added functionality. Options for eating apples, roll for new defense / attack stats, along with some constructor changes.

--- src/entity.py
+++ src/entity.py
@@ -2,25 +2,40 @@ import colored
 import random
 
 class entity(object):
-    # global variables
+    # global variables // might wanna remove these
     name = "entity"
-    health = 100
-    damage = random.randrange(1, 100)
-    defense = random.randrange(1, 50)
+    
     
 # instantiate the entity object
-    def __init__(self, name, health, damage, defend):
+    def __init__(self, name = "Default Entity", health = 100, damage = random.randrange(1, 100), defense = random.randrange(1, 100)):
         self.name = name
         self.health = health
         self.damage = damage
-        self.defend = defend
+        self.defense = defense
 
     def render_status(self):
         print("\n\nName: ", self.name)        
         print("Health: ", self.health) 
         print("Damage: ", self.damage) 
-        print("Defense: ", self.defend) 
+        print("Defense: ", self.defense) 
 
-    def take_damage(self):
+    def take_damage(self, arg_damage_taken):
         self.health = self.health - self.damage
         print("remaining health: ", self.health)
+
+# A function for damage and defense must exist. Having --random.randrange(1, 100)-- does not work properly when stored as an entity variable. 
+# Once the object is initialized, the random integer is stored permenantly. Each entity needs to have a random amount of damage for each round.
+    def roll_for_damage(self):
+        self.damage = random.randrange(1, 100)
+        print("New Damage Stat: ", self.damage)
+
+    def roll_for_defense(self):
+        self.defense = random.randrange(1, 100)
+        print("New Defense Stat: ", self.defense)
+
+    def eat_apple(self):
+        calculated_health_buff = self.health + random.randrange(-25, 200)
+        print("calculated_health_buff: ", calculated_health_buff)
+        print('mmm, that was tasty. ', self.health, "+", calculated_health_buff, "hp")
+        self.health = calculated_health_buff
+        print(self.health)
