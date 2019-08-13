4c9fd4fea1aadd92d9c7cb37cf5858bcf76bd1db
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-13 19:53:50 -0400

    Update run.py

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ gm.render_status()
 
 while gm.player_1.health > 0 and gm.enemy_ai.health > 0:
     gm.game_over = False 
+    gm.enemy_threat_dmg()
     time.sleep(1)
     gm.enemy_do_dmg(gm.player_1)
     time.sleep(1)
