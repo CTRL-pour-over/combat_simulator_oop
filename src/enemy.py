f2e3bdc49ac47a0d4eb50a4d4552782709379914
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-22 12:23:07 -0400

    Update enemy.py
    enemy now has its own option menu, which is inherited and overrode from entity. added documentation

--- src/enemy.py
+++ src/enemy.py
@@ -1,22 +1,33 @@
 from src.entity import entity
 from colored import fg
-
+import random
 class enemy_child(entity):
-    # global variables might be unnessesary
-    name = "Enemy"
-
-    def __init__(self, arg_name = "", arg_health = 100, arg_damage = 50, arg_defense = 100):
+    # default values for enemy, these get overwritten in the constructor, Inside the Managers __init__ function
+    def __init__(self, arg_color = "%s" % fg(1), arg_name = "Enemy", arg_health = 100, arg_damage = 50, arg_defense = 100):
+        self.color = arg_color
         self.name = arg_name
         self.health = arg_health
         self.damage = arg_damage
         self.defense = arg_defense
-
+    # prints enemys stats to screen, gets called in the managers render_status
     def render_status(self):
-        print("\n%sName: " % fg(1), self.name)        
-        print("%sHealth: "% fg(1), self.health) 
-        print("%sDamage: "% fg(1), self.damage) 
-        print("%sDefense: "% fg(1), self.defense) 
-
-    def takes_his_turn(self):
-        pass
-
+        print(self.color)
+        print("\nName: ", self.name)        
+        print("Health: ", self.health) 
+        print("Damage: ", self.damage) 
+        print("Defense: ", self.defense) 
+    # option menu for the enemy. this function is called in Managers "enemy_take_turn" function.
+    def MakeChoice(self, arg_player):
+        option_select = random.randrange(1, 5)
+        if option_select == 1:
+            self.violate_opponent(self, arg_player)
+        elif option_select == 2:
+            self.defend_incoming_attack(self)
+        elif option_select == 3:
+            self.roll_for_damage(self)
+        elif option_select == 4:
+            self.roll_for_defense(self)
+        elif option_select == 5:
+            self.eat_apple(self)
+        else:
+            print("This couldnt possibly happen")
