9428b0c349ee1427d56e1a5fcbcfe01402b2cfcb
smoothpocket <41839907+smoothpocket@users.noreply.github.com>
2019-08-22 12:21:05 -0400

    Update run.py
    Simplified main game loop, added documentation.

--- run.py
+++ run.py
@@ -4,25 +4,31 @@ from src.manager import Manager
 import time
 from colored import fg, bg, attr
 
-#tree: ============ entity_class ===============
-# ------ player_child -------- enemy_child -------
-# ----- attk / hp --------------- attk / hp -------
+###########ORIGINAL_DESIGN_ARCHITECTURE##############
+#tree: ============ entity_class ====================
+# ------ player_child -------- enemy_child ----------
+# ----- attk / hp --------------- attk / hp ---------
 # ===================================================
-gm = Manager()
-gm.game_start()
-gm.render_status()
 
+# renaming the manager class
+gm = Manager()
+# printing welcome screen
+gm.print_screen(gm.welcome_screen)
+#vVvVvVvVvVv MAIN GAME LOOP, GAME IS RUN HERE vVvVvVvV##
+### gm.render_status, gm.player_take_turn, gm.enemy_take_turn are functions that live in Manager. ###
 while gm.player_1.health > 0 and gm.enemy_ai.health > 0:
     gm.game_over = False 
-    gm.enemy_threat_dmg()
-    time.sleep(1)
-    gm.input_management()
-    time.sleep(1)
-    gm.enemy_do_dmg(gm.player_1)
-    time.sleep(1)
+
     gm.render_status()
     time.sleep(1)
+    gm.player_take_turn()
+    time.sleep(2)
+    gm.enemy_take_turn()
+    time.sleep(2)
 gm.game_over = True
+
+# game over logic, just figures out which loading screen to play   #
+# everything that starts with gm is coming from the Manager class. #
 while gm.game_over == True:
     if gm.player_1.health > 0:
         gm.print_screen(gm.winner_end_screen)
@@ -30,4 +36,3 @@ while gm.game_over == True:
         gm.print_screen(gm.loser_end_screen)
     else:
         print(gm.print_screen("\nYou both fall before your knees, regret. You question whether or not the fight was worth the cause, your life. As you both bleed out, you each take a look into one anothers eyes, knowing it will be the last moment of your lives, a shared experience, none the less.\n\n"))
-    
